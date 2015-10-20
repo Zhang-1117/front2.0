62c83a14e38efc4124ab5af433c25c8b5267f63e
zxl <zhangxiaolu@yovole.com>
2015-10-20 09:14:07 +0800

    add host page

---
+++ modules/host/config/host.routes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('host').config(['$stateProvider', '$urlRouterProvider',
+	function ($stateProvider, $urlRouterProvider) {
+		$stateProvider.
+		state('host', {
+			url: '/host',
+			templateUrl: 'host/host.html'
+		});
+	}]);
